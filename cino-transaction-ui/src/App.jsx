c45ec0225844ebab37d31bc8f039c6b710274bb4
Hiroshi Kuboe <kuboe.hiro@gmail.com>
2025-03-31 20:30:51 +0200

    Add files via upload

---
+++ cino-transaction-ui/src/App.jsx
@@ -0,0 +1,13 @@
+
+import TransactionHistory from "./TransactionHistory";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white p-4">
+      <h1 className="text-xl font-bold mb-4">取引履歴</h1>
+      <TransactionHistory />
+    </div>
+  );
+}
+
+export default App;
